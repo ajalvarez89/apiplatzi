d33cfd751a4a3667bc5b08b30db5b790f276ef42
Alvaro Alvarez <aalvareza@energiasolarsa.com>
2019-07-22 17:06:04 -0500

    I'm going here! chapter 14

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   get '/health', to: 'health#health'
 
-  resources :posts, only: [:index, :show]
+  resources :posts, only: [:index, :show, :create, :update]
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
